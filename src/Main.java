136a6f9cd8d83d78e6260d9d2b971bff33f2b239
melvinsibichen <melvinsibichen1020@gmail.com>
2022-11-22 07:29:20 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println("------------------ Menu ----------------------");
+        System.out.println("1. Add Student");
+        System.out.println("2. View Students");
+        System.out.println("3. Search Student");
+        System.out.println("4. Delete Student");
+        System.out.println("5. Exit");
+        System.out.println("----------------------------------------------");
+        System.out.print("\nPlease enter the choice: ");
+    }
+}
